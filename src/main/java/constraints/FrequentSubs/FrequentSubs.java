03045fa11861664fa04e02cfb5f6137fddc669cf
Bachir Belaid <bachir@simula.no>
2021-07-10 09:24:21 +0000

    FrequentSubs constraint

---
+++ src/main/java/constraints/FrequentSubs/FrequentSubs.java
@@ -0,0 +1,31 @@
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.constraints.Propagator;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.IntVar;
+
+/**
+ * 
+ * @author bachir 28/11/2018
+ */
+public class FrequentSubs extends Constraint {
+
+	/*******************************************
+	 * 
+	 * @author bachir
+	 * @since 04/2017
+	 * @param X
+	 *            binary item variables
+	 * @param s
+	 *            a relative frequency (%)
+	 * @param d
+	 *            Datset
+	 * 
+	 */
+	public FrequentSubs(BoolVar[] X, double s, Dataset d) {
+		super("FrequentSubs", new PropFrequentSubs(X, s, d));
+		//super("ClosedPattern", new PropClosedPatternSparse(Z, teta, d));
+	}
+
+}
