10d9205469f1750265acdb13ce3b1c54f180ad2d
Bachir Belaid <bachir@simula.no>
2021-07-10 09:20:49 +0000

    Generator constraint

---
+++ src/main/java/constraints/Generator/Generator.java
@@ -0,0 +1,34 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.constraints.Propagator;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.IntVar;
+/**
+ * 
+ * @author bachir
+ *12/04/2017
+ */
+public class Generator extends Constraint {
+	
+	/*******************************************
+	 * 
+	 * 
+	 * @param vars binary item variables
+	 * @param teta  a relative frequency (%)
+	 * @param d	datset 
+	 * 
+	 */
+	public Generator(BoolVar[] X,double teta, Dataset d) {
+		super("Generator", new PropGeneratorIjcai(X,teta,d));
+		//super("Generator", new PropGeneratorBacktrackableV2(X,teta,d));
+		//super("Generator", new PropGenerator(X,teta,d));
+		//super("Generator", new PropGeneratorV1Plus(X,teta,d));
+		//super("Generator", new PropGeneratorMemory(X,teta,d));
+		//super("Generator", new PropGeneratorInverted(X,teta,d));
+		//super("Generator", new PropGeneratorSparse(X,teta,d));
+		
+		
+	}
+
+}
+
