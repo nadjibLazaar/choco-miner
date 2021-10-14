9f35bc8d49e8ad43642fe25bdbe13b7e52f01822
Bachir Belaid <bachir@simula.no>
2021-10-14 13:09:20 +0000

    Infrequent constraint

---
+++ src/main/java/core/constraints/Infrequent/Infrequent.java
@@ -0,0 +1,26 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.constraints.Constraint;
+import org.chocosolver.solver.constraints.Propagator;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.IntVar;
+/**
+ * 
+ * @author bachir
+ *12/04/2017
+ */
+public class Infrequent extends Constraint {
+	
+	/*******************************************
+	 * 
+	 * 
+	 * @param vars binary item variables
+	 * @param teta  a relative frequency (%)
+	 * @param d	datset 
+	 * 
+	 */
+	public Infrequent(BoolVar[] X,double teta, Dataset d) {
+		super("Infrequent", new PropInfrequent(X,teta,d));
+	}
+
+}
+
