d54520a35008a2a93b5ee3a20c0347ac6049e27c
LunaticCat <yannbrun1507@outlook.fr>
2021-11-11 19:30:15 +0100

    Small changes

--- src/main/test/ClosedChecker.java
+++ src/main/test/ClosedChecker.java
@@ -10,9 +10,11 @@ public class ClosedChecker {
     @Test
     public void closedCheckerTest() throws IOException {
         ArrayList<DataObjectLine> spmfResultLazaar = TestUtil.parser("spmf-outputs/lazaar-results");
-        // TODO launch ChocoMiner to update result
         System.out.println(spmfResultLazaar); //Temporaire, verifie que le parser fonctionne
-        //ArrayList<DataObjectLine> chocoMinerResultLazaar = TestUtil.parser(""); //TODO change to take instance of lazaar for chocominer
-
+        /*
+        TODO launch ChocoMiner to update result
+        ArrayList<DataObjectLine> chocoMinerResultLazaar = TestUtil.parser("");
+        assertTrue(condition);
+         */
     }
 }
